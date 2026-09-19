c0916d251d673be6f49869a297537e3bc9c30333
AdemEmin <adememin2020@gmail.com>
2026-09-19 19:21:43 +0300

    bayagı dosya eklendi

---
+++ app/urunler/page.tsx
@@ -0,0 +1,20 @@
+import Ust_baslik from "../../component/ust_baslik";
+
+export default function Items() {
+  return (
+    <div>
+      <Ust_baslik />
+
+      <main>
+        <a>deneme</a>
+        <a>deneme</a>
+
+        <q>deneme</q>
+
+        <a>deneme</a>
+        <a>deneme</a>
+        <a>deneme</a>
+      </main>
+    </div>
+  );
+}
